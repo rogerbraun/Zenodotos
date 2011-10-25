78c2d3104fb4d0b4fda50881ad2fcb8a165f9002
Roger Braun <roger@rogerbraun.net>
2011-10-25 14:08:28 +0200

    standard rails 3.1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenodotos::Application.config.secret_token = 'f711df744116f809a3c1edd673cda549ba2790fd6f0b1030afa5b8035e0bbc0552b3c5b82af30ddaf977b46cf3510823c480f54253f47c987474965185927586'
