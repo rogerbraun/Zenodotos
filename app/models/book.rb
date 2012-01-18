ba3717a197fe0a6ef68555f3d53d4fe68861ac00
Roger Braun <roger@rogerbraun.net>
2012-01-18 16:42:19 +0100

    add default values for book. Fixes #13

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@ class Book < ActiveRecord::Base
   paginates_per 10
   has_many :lendings
 
+  after_initialize :init
+
   def current_lending
     Lending.find_by_book_id_and_returned(id, false)
   end
@@ -10,6 +12,13 @@ class Book < ActiveRecord::Base
   def self.quicksearch q
     query = "%#{q}%"
     Book.where("autor like ? or autor_japanisch like ? or titel like ? or titel_japanisch like ?", query, query, query, query)
+  end
+
+  private
+  
+  def init
+    self.signatur = "Signatur folgt."
+    self.aufnahmedatum = Date.today
   end
   
 end
