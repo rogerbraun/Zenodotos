c1459c7b5a3bcebfb51122906d74f5003d166bcf
Roger Braun <roger@rogerbraun.net>
2012-01-24 17:39:33 +0100

    second try for db fix

--- config/initializers/picky.rb
+++ config/initializers/picky.rb
@@ -1,23 +1,25 @@
 # Build initial index
 # Book.reindex
 #
+if Book.table_exists?  then 
+  BookIndex = Picky::Index.new :books do
+    source Book.all
+    backend Picky::Backends::SQLite.new{realtime:true}
 
-BookIndex = Picky::Index.new :books do
-  backend Picky::Backends::SQLite.new{realtime:true}
-
-  Book.attribute_names[1..-1].each do |cname|
-    category cname.to_sym
+    Book.attribute_names[1..-1].each do |cname|
+      category cname.to_sym
+    end
+    indexing  removes_characters: /[^\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z0-9\. ]/u,
+              substitutes_characters_with: Picky::CharacterSubstituters::WestEuropean.new,
+              splits_text_on: /\s/u
   end
-  indexing  removes_characters: /[^\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z0-9\. ]/u,
-            substitutes_characters_with: Picky::CharacterSubstituters::WestEuropean.new,
-            splits_text_on: /\s/u
+
+  BookSearch = Picky::Search.new BookIndex do
+    searching removes_characters: /[^\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z0-9\.: ]/u,
+              substitutes_characters_with: Picky::CharacterSubstituters::WestEuropean.new,
+              splits_text_on: /\s/u
+
+  end
+
+  BookIndex.load
 end
-
-BookSearch = Picky::Search.new BookIndex do
-  searching removes_characters: /[^\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z0-9\.: ]/u,
-            substitutes_characters_with: Picky::CharacterSubstituters::WestEuropean.new,
-            splits_text_on: /\s/u
-
-end
-
-BookIndex.load
