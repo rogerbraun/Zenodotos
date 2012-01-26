a500e5ff98e4a3f6ded49c00bec3537e42c36ffc
Roger Braun <davinelulinvega@gmail.com>
2012-01-26 22:38:04 +0100

    remove more active admin

--- app/assets/javascripts/active_admin.js
+++
@@ -1,6 +0,0 @@
-//= require active_admin/base
-
-$(function() {
-  $("#book_aufnahmedatum").datepicker({dateFormat: 'yy-mm-dd'});
-  $("#lending_return_date").datepicker({dateFormat: 'yy-mm-dd'});
-});
