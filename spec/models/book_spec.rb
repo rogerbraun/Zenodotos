d850690a8b4edfa5d962dabeb3bc300c30128e11
Roger Braun <roger@rogerbraun.net>
2012-01-27 11:52:12 +0100

    fix search spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,7 +6,7 @@ describe Book do
 
   it "should be able to do a one-param quicksearch" do
     Factory(:book, :titel => "Ein ungewöhnlicher Titel")
-    @books = Book.quicksearch("ungewöhnlich")
+    @books = Book.search("ungewöhnlich")
     @books.size.should == 1
   end
 
