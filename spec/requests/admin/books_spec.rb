12aed4888e83de294d24a6000a21556f503d987d
Roger Braun <roger@rogerbraun.net>
2012-01-31 18:11:04 +0100

    more specs

--- spec/requests/admin/books_spec.rb
+++ spec/requests/admin/books_spec.rb
@@ -12,10 +12,32 @@ describe Admin do
       end
     end
 
-    it "displays all books if nothing is searched" do
-      visit admin_books_path
-      Book.limit(10).each do |book|
-        page.should have_content(book.titel)
+    describe "Index Page" do
+      before do
+        visit admin_book_path
+      end
+
+      it "displays all books if nothing is searched" do
+        Book.limit(10).each do |book|
+          page.should have_content(book.titel)
+        end
+      end
+
+      it "can make new books" do
+        page.should have_selector("#new_book_button")  
+        click_on "new_book_button"
+        page.should have_content("Neues Buch")
+      end
+    end
+
+    describe "Member Page" do
+      before do
+        @book = Book.first
+        visit admin_book_path(@book)
+      end
+        
+      it "displays a single book" do
+        page.should have_selector("input[value='#{@book.titel}']")
       end
     end
   end
