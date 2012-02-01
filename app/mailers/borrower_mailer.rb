e905d32f7134083395e1ea91ac310e53ec90f227
Roger Braun <davinelulinvega@gmail.com>
2012-02-01 21:42:48 +0100

    add cc to mails

--- app/mailers/borrower_mailer.rb
+++ app/mailers/borrower_mailer.rb
@@ -7,6 +7,6 @@ class BorrowerMailer < ActionMailer::Base
   def overdue_reminder(borrower, lendings)
     @name = borrower.name
     @lendings = lendings
-    mail :to => borrower.email, :subject => "Bücher sind überfallig" 
+    mail :to => borrower.email, :cc => "bibliothek@japanologie.uni-tuebingen.de", :subject => "Bücher sind überfallig" 
   end
 end
