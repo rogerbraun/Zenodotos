b3ceddcc2640fe293d6d6bf55c345c53bb19fca7
Roger Braun <roger@rogerbraun.net>
2012-02-03 11:21:08 +0100

    sidebar for new

--- db/schema.rb
+++ db/schema.rb
@@ -13,21 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120202205704) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.integer  "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
   create_table "admin_users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
@@ -39,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20120202205704) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
@@ -113,8 +98,8 @@ ActiveRecord::Schema.define(:version => 20120202205704) do
     t.string   "telefon2"
     t.string   "ub_nr"
     t.string   "vermerke"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "collections", :force => true do |t|
@@ -126,8 +111,8 @@ ActiveRecord::Schema.define(:version => 20120202205704) do
     t.integer  "borrower_id"
     t.integer  "book_id"
     t.boolean  "returned",    :default => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.integer  "printout_id"
   end
 
@@ -140,8 +125,8 @@ ActiveRecord::Schema.define(:version => 20120202205704) do
   end
 
   create_table "printouts", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "reminders", :force => true do |t|
