01487e6dfa5434735e5ae96bf20910cff726e00e
thebluber <thebluber@googlemail.com>
2013-02-11 00:09:26 +0100

    display the result length of a query in user interface

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@
 class SearchController < ApplicationController
   def index
     @page = params[:page] || 1
+    @total_amount = Book.search(params[:search]).length
     @books = Book.search(params[:search]).page(@page)
   end
 
