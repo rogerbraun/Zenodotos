a26be56ead44bf36b058054b084d6f7e7b3b4a75
thebluber <thebluber@googlemail.com>
2013-02-11 13:52:06 +0100

    new start page

--- config/routes.rb
+++ config/routes.rb
@@ -106,7 +106,7 @@ Zenodotos::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'search#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
